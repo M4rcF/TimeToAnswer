3730bcc666bba03f5c765dacdf1d6414b68d206b
M4rcF <m4rcusfiuza@hotmail.com>
2021-09-28 22:56:21 +0000

    pt 3

--- app/controllers/admins_w/admins_controller.rb
+++ app/controllers/admins_w/admins_controller.rb
@@ -1,5 +1,47 @@
 class AdminsW::AdminsController < AdminsWController
+  before_action :set_id, only: [:edit, :update, :destroy]
+  before_action :set_params, only: [:update, :create]
+
   def index
     @admins = Admin.all
+  end
+
+  def edit
+  end
+
+  def update
+    if @admin.update(set_params)
+      redirect_to admins_w_admins_path, notice: "Administrador atualizado"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @admin.destroy
+    redirect_to admins_w_admins_path
+  end
+
+  def new
+    @admin = Admin.new
+  end
+
+  def create
+    @admin = Admin.new(set_params)
+    if @admin.save
+      redirect_to admins_w_admins_path
+    else
+      render :new
+    end
+  end
+  
+  private
+
+  def set_id
+    @admin = Admin.find(params[:id])
+  end
+
+  def set_params
+    params.require(:admin).permit(:email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  namespace :admins_w do
-    get 'admins/index'
-  end
   namespace :site do
     get 'welcome/index'
   end
   namespace :admins_w do
     get 'welcome/index'
+    resources :admins, only: [:index, :edit, :update, :destroy, :new, :create]
   end
   namespace :users_w do
     get 'welcome/index'
