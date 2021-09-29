1d2238c8382d387c5f7f7cab6a0c6dde52e46790
M4rcF <m4rcusfiuza@hotmail.com>
2021-09-29 13:02:39 +0000

    pt 4

--- app/controllers/admins_w/admins_controller.rb
+++ app/controllers/admins_w/admins_controller.rb
@@ -1,15 +1,17 @@
 class AdminsW::AdminsController < AdminsWController
   before_action :set_id, only: [:edit, :update, :destroy]
   before_action :set_params, only: [:update, :create]
+  before_action :set_verify_password, only: [:update]
 
   def index
-    @admins = Admin.all
+    @admins = Admin.all.page(params[:page]).per(5)
   end
 
   def edit
   end
 
   def update
+    
     if @admin.update(set_params)
       redirect_to admins_w_admins_path, notice: "Administrador atualizado"
     else
@@ -19,7 +21,7 @@ class AdminsW::AdminsController < AdminsWController
 
   def destroy
     @admin.destroy
-    redirect_to admins_w_admins_path
+    redirect_to admins_w_admins_path, notice: "Administrador excluído"
   end
 
   def new
@@ -29,7 +31,7 @@ class AdminsW::AdminsController < AdminsWController
   def create
     @admin = Admin.new(set_params)
     if @admin.save
-      redirect_to admins_w_admins_path
+      redirect_to admins_w_admins_path, notice: "Administrador criado"
     else
       render :new
     end
@@ -43,5 +45,11 @@ class AdminsW::AdminsController < AdminsWController
 
   def set_params
     params.require(:admin).permit(:email, :password, :password_confirmation)
+  end
+
+  def set_verify_password
+    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
+      params[:admin].extract!(:password, :password_confirmation)
+    end
   end
 end
